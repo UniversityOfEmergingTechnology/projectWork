8e3a2e67b9a8455fee5c1de1b32384b6705c98fd
Mudit Kapoor <muditk441@gmail.com>
2023-07-11 22:06:53 +0530

    made some changes in mail templates

--- server/mail/templates/contactFormRes.js
+++ server/mail/templates/contactFormRes.js
@@ -71,8 +71,8 @@ exports.contactUsEmail = (
     
     <body>
         <div class="container">
-            <a href="https://studynotion-edtech-project.vercel.app"><img class="logo"
-                    src="https://i.ibb.co/7Xyj3PC/logo.png" alt="StudyNotion Logo"></a>
+            <a href="https://Uniemtech-edtech-project.vercel.app"><img class="logo"
+                    src="https://asset.cloudinary.com/dylkdmcsl/1b7d032736e1d9427283942e78ba5fc1" alt="Uniemtech Logo"></a>
             <div class="message">Contact Form Confirmation</div>
             <div class="body">
                 <p>Dear ${name},</p>
@@ -85,7 +85,7 @@ exports.contactUsEmail = (
                 <p>We appreciate your interest and will get back to you shortly. </p>
             </div>
             <div class="support">If you have any further questions or need immediate assistance,    please feel free to reach
-                out to us at <a href="mailto:info@studynotion.com">info@studynotion.com</a>. We are here to help!</div>
+                out to us at <a href="mailto:info@Uniemtech.com">info@Uniemtech.com</a>. We are here to help!</div>
         </div>
     </body>
     
--- server/mail/templates/passwordUpdate.js
+++ server/mail/templates/passwordUpdate.js
@@ -55,8 +55,8 @@ exports.passwordUpdated = (email, name) => {
     
     <body>
         <div class="container">
-            <a href="https://studynotion-edtech-project.vercel.app"><img class="logo"
-                    src="https://i.ibb.co/7Xyj3PC/logo.png" alt="StudyNotion Logo"></a>
+            <a href="https://Uniemtech-edtech-project.vercel.app"><img class="logo"
+                    src="https://asset.cloudinary.com/dylkdmcsl/1b7d032736e1d9427283942e78ba5fc1" alt="Uniemtech Logo"></a>
             <div class="message">Password Update Confirmation</div>
             <div class="body">
                 <p>Hey ${name},</p>
@@ -66,7 +66,7 @@ exports.passwordUpdated = (email, name) => {
             </div>
             <div class="support">If you have any questions or need further assistance, please feel free to reach out to us
                 at
-                <a href="mailto:info@studynotion.com">info@studynotion.com</a>. We are here to help!
+                <a href="mailto:info@Uniemtech.com">info@Uniemtech.com</a>. We are here to help!
             </div>
         </div>
     </body>
